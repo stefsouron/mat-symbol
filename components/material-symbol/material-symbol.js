dbc90813c74fd256fcea5884f3d3ae5a64b22d6e
Stephane Souron <stephane.souron@gmail.com>
2024-10-27 21:35:55 +0100

    icon name in mat-icon textContent

--- components/material-symbol/material-symbol.js
+++ components/material-symbol/material-symbol.js
@@ -3,16 +3,26 @@ export class MaterialSymbol extends HTMLElement {
 
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: 'open' })
     this.shadowRoot.innerHTML = /*html*/`
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
       <span class="material-symbols-outlined"></span>
-    `;
+    `
   }
 
   connectedCallback() {
-    this.#iconElement = this.shadowRoot.querySelector('.material-symbols-outlined');
-    this.#iconElement.innerText = this.getAttribute('name') || 'home';
+    /* The components auto-adds (once) the needed stylesheet
+       to the document's head, or it doesn't work. */
+    const isStyleSheetAdded = Boolean(document.getElementById('material-symbols-stylesheet'))
+    if (!isStyleSheetAdded) {
+      const docLink = document.createElement('link')
+      docLink.id = 'material-symbols-stylesheet'
+      docLink.rel = 'stylesheet'
+      docLink.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0'
+      document.head.appendChild(docLink)
+    }
+    this.#iconElement = this.shadowRoot.querySelector('.material-symbols-outlined')
+    this.#iconElement.innerText = this.textContent || 'home'
   }
 }
 
