2ae9261bd6181244e49607912bf42fb058f2e741
Stephane Souron <stephane.souron@gmail.com>
2024-10-27 21:11:47 +0100

    working in shadow DOM

--- components/material-symbol/material-symbol.js
+++ components/material-symbol/material-symbol.js
@@ -3,16 +3,17 @@ export class MaterialSymbol extends HTMLElement {
 
   constructor() {
     super();
-    this.innerHTML = /*html*/`
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = /*html*/`
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
       <span class="material-symbols-outlined"></span>
     `;
   }
 
   connectedCallback() {
-    this.#iconElement = this.querySelector('.material-symbols-outlined');
+    this.#iconElement = this.shadowRoot.querySelector('.material-symbols-outlined');
     this.#iconElement.innerText = this.getAttribute('name') || 'home';
   }
 }
 
-customElements.define('material-symbol', MaterialSymbol);
+customElements.define('mat-symbol', MaterialSymbol);
