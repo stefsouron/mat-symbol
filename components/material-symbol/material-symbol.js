18f51b10e0a3e828013fe902a002f1a394ac9d54
Stephane Souron <stephane.souron@gmail.com>
2024-10-27 22:30:56 +0100

    size, color and name filtering

--- components/material-symbol/material-symbol.js
+++ components/material-symbol/material-symbol.js
@@ -1,5 +1,7 @@
+import symbolNames from './material-symbols-name.js'
+
 export class MaterialSymbol extends HTMLElement {
-  #iconElement = null;
+  #sizeMatchRegex = /^-?\d+(\.\d+)?(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)?$/
 
   constructor() {
     super();
@@ -21,8 +23,13 @@ export class MaterialSymbol extends HTMLElement {
       docLink.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0'
       document.head.appendChild(docLink)
     }
-    this.#iconElement = this.shadowRoot.querySelector('.material-symbols-outlined')
-    this.#iconElement.innerText = this.textContent || 'home'
+    const iconElement = this.shadowRoot.querySelector('.material-symbols-outlined')
+    const size = this.getAttribute('size') ?? ''
+    const color = this.getAttribute('color') ?? 'inherit'
+    iconElement.style.fontSize = this.#sizeMatchRegex.test(size) ? size : '1.5rem'
+    iconElement.style.color = color
+    const givenName = this.textContent.toLowerCase().replace(/\s+/g, '_')
+    iconElement.innerText = (symbolNames.includes(givenName)) ? givenName : 'home'
   }
 }
 
